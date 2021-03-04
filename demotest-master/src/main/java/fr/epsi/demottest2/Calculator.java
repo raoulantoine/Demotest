1f029339d15f6a009918db9a4fd93f1a84849b7b
raoulantoine <57132057+raoulantoine@users.noreply.github.com>
2021-03-04 14:17:36 +0100

    Add files via upload

---
+++ demotest-master/src/main/java/fr/epsi/demottest2/Calculator.java
@@ -0,0 +1,13 @@
+package fr.epsi.demottest2;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+}
